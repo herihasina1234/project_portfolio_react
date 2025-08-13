9e5245fc863ac3d4bce7e8408b3971686b10144e
Venance Herihasina <rasamimananaherihasina@gmail.com>
2025-08-13 19:22:24 +0300

    feat Contact: Building contact form component

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -1,4 +1,4 @@
-// import { RevealOnScroll } from "../RevealOnScroll";
+import { RevealOnScroll } from "../RevealOnScroll";
 
 export const Projects = () => {
   return (
@@ -6,7 +6,7 @@ export const Projects = () => {
       id="projects"
       className="min-h-screen flex items-center justify-center py-20"
     >
-        {/* <RevealOnScroll> */}
+        <RevealOnScroll>
             <div className="max-w-5xl mx-auto px-4">
                 <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
                 {" "}
@@ -42,7 +42,7 @@ export const Projects = () => {
                     </div>                                    
                 </div>
             </div>
-        {/* </RevealOnScroll> */}
+        </RevealOnScroll>
     </section>
   );
 };
